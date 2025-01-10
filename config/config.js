c0242c6a8d3abeea63ee33e82410276f0e4d67ad
Branch Manager <branch@branch.bet>
2025-01-10 13:59:42 -0500

    beyond no return

--- config/config.js
+++ config/config.js
@@ -3,8 +3,9 @@ dotenv.config();
 
 export const config = {
   // DegenDuel API is running on port 3003
-  port: process.env.PORT || 3003,
-  environment: process.env.NODE_ENV || 'production',
+  port: process.env.PORT || process.env.API_PORT || 3004,
+  env: process.env.NODE_ENV || 'production',
+  debug_mode: process.env.DD_API_DEBUG_MODE || 'false',
   jwt: {
     secret: process.env.JWT_SECRET
   }
