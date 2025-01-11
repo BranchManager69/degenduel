43d3f6688cafab8e3f33dbb1091690949679ca77
Branch Manager <branch@branch.bet>
2025-01-11 03:25:04 -0500

    bbb

--- config/config.js
+++ config/config.js
@@ -2,11 +2,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  // DegenDuel API is running on port 3003 or 3004 idk
   port: process.env.PORT || process.env.API_PORT || 3003,
   env: process.env.NODE_ENV || 'production',
   debug_mode: process.env.DD_API_DEBUG_MODE || 'false',
   jwt: {
-    secret: process.env.JWT_SECRET
+    secret: process.env.JWT_SECRET,
+    superadmin_secret: process.env.SUPERADMIN_TOKEN
   }
 };
