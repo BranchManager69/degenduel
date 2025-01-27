998dd08011d72542f03edf00c01379a5096f4b5e
Branch Manager <branch@branch.bet>
2025-01-27 04:00:04 -0500

    DD 0.2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -12,6 +12,23 @@ module.exports = {
         PORT: process.env.PORT || 3004,
         DD_API_DEBUG_MODE: process.env.DD_API_DEBUG_MODE || 'false'
       }
+    },
+    {
+      name: 'prisma-studio',
+      script: 'npx',
+      args: 'prisma studio',
+      watch: false,
+      cwd: '/home/branchmanager/websites/degenduel',
+      env: {
+        PORT: 5555
+      }
+    },
+    {
+      name: 'pgadmin4',
+      script: 'docker',
+      args: 'start pgadmin4',
+      watch: false,
+      autorestart: true
     }
   ]
 };
