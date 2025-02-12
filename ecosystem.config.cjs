905c929930f74b2916b5d1dca67582faba247f84
Branch Manager <branch@branch.bet>
2025-02-11 22:41:53 -0500

    Ecosystems 2.0

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -26,6 +26,29 @@ module.exports = {
       kill_timeout: 3000
     },
     {
+      name: 'degenduel-api-test',
+      script: 'index.js',
+      watch: true,
+      cwd: '/home/branchmanager/websites/degenduel',
+      wait_ready: true,
+      node_args: [
+        '--no-warnings',
+        '--experimental-specifier-resolution=node',
+        '--optimize-for-size',
+        '--gc-interval=100'
+      ],
+      env: {
+        PORT: 3005,
+        DD_API_DEBUG_MODE: 'true',
+        NODE_ENV: 'development',
+        NODE_OPTIONS: '--require ts-node/register',
+        DATABASE_URL: process.env.DATABASE_URL_TEST // Will use test database
+      },
+      exp_backoff_restart_delay: 100,
+      max_memory_restart: '2G',
+      kill_timeout: 3000
+    },
+    {
       name: 'prisma-studio',
       script: 'npx',
       args: 'prisma studio',
@@ -33,6 +56,17 @@ module.exports = {
       cwd: '/home/branchmanager/websites/degenduel',
       env: {
         PORT: 5555
+      }
+    },
+    {
+      name: 'prisma-studio-test',
+      script: 'npx',
+      args: 'prisma studio',
+      watch: false,
+      cwd: '/home/branchmanager/websites/degenduel',
+      env: {
+        PORT: 5556,
+        DATABASE_URL: process.env.DATABASE_URL_TEST // Will use test database
       }
     },
     {
