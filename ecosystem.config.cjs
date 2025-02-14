96eb924e4da7e8e0e01c0447a9645fc0e27e598a
Branch Manager <branch@branch.bet>
2025-02-14 08:15:23 -0500

    Fkd up ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -16,33 +16,10 @@ module.exports = {
         '--gc-interval=100'
       ],
       env: {
-        PORT: process.env.PORT || 3004,
+        PORT: 3004,
         DD_API_DEBUG_MODE: 'false',
         NODE_ENV: 'production',
         NODE_OPTIONS: '--require ts-node/register'
-      },
-      exp_backoff_restart_delay: 100,
-      max_memory_restart: '2G',
-      kill_timeout: 3000
-    },
-    {
-      name: 'degenduel-api-test',
-      script: 'index.js',
-      watch: true,
-      cwd: '/home/branchmanager/websites/degenduel',
-      wait_ready: true,
-      node_args: [
-        '--no-warnings',
-        '--experimental-specifier-resolution=node',
-        '--optimize-for-size',
-        '--gc-interval=100'
-      ],
-      env: {
-        PORT: 3005,
-        DD_API_DEBUG_MODE: 'true',
-        NODE_ENV: 'development',
-        NODE_OPTIONS: '--require ts-node/register',
-        DATABASE_URL: process.env.DATABASE_URL_TEST // Will use test database
       },
       exp_backoff_restart_delay: 100,
       max_memory_restart: '2G',
